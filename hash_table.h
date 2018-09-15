d87e2845b031cfa20458dcaabf2af1820c79b783
HuffCoders <42897009+HBMTP@users.noreply.github.com>
2018-09-15 17:23:34 -0300

    hash_table.h v2.0

--- hash_table.h
+++ hash_table.h
@@ -4,8 +4,9 @@ typedef struct hash_table hash_table;
 typedef struct element element;
 hash_table* create_hash_table();
 int create_index(unsigned char key);
-void put(hash_table *ht, unsigned char key, unsigned char value);
+void put(hash_table *ht, unsigned char key);
 int get(hash_table *ht, unsigned char key);
 void remove_ht(hash_table *ht, unsigned char key);
+void print_hs(hash_table *ht);
 
 #endif
